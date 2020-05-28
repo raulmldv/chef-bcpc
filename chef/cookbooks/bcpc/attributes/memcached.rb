8e8710e636ab9f6b50443b810a78e179df5d1a18
npustchi <npustchi@gmail.com>
2020-05-28 16:41:57 -0400

    Make memcached memory limit a prop tunable.
    Signed-off-by: npustchi <npustchi@bloomberg.net>

---
+++ chef/cookbooks/bcpc/attributes/memcached.rb
@@ -0,0 +1,7 @@
+###############################################################################
+# Memcached
+###############################################################################
+
+# Specifies memcached maximum limit of RAM to use for item
+# storage (in megabytes). Note carefully that this isn't a global memory limit
+default['bcpc']['memcached']['memory'] = 1024
