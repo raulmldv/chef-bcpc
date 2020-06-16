db61c7fe677aaef8fb062ece4bea70ec7a00e0a6
Tyler Stachecki <tstachecki@bloomberg.net>
2020-06-16 17:04:17 -0400

    Set default memcached memory limit to 64MB.
    This commit sets the default attribute for memcached's
    memory limit back to Ubuntu's packaging defaults (64MB).
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/memcached.rb
+++ chef/cookbooks/bcpc/attributes/memcached.rb
@@ -4,4 +4,4 @@
 
 # Specifies memcached maximum limit of RAM to use for item
 # storage (in megabytes). Note carefully that this isn't a global memory limit
-default['bcpc']['memcached']['memory'] = 1024
+default['bcpc']['memcached']['memory'] = 64
