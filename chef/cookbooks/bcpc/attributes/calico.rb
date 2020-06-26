d7de9d66371a2c8d87c391c94c2004a2f3989623
David Comay <dcomay@bloomberg.net>
2020-06-26 16:39:11 -0400

    Calico 3.15, other related changes/cleanup
    Signed-off-by: David Comay <dcomay@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/calico.rb
+++ chef/cookbooks/bcpc/attributes/calico.rb
@@ -4,7 +4,7 @@
 
 # calico apt repository
 default['bcpc']['calico']['repo']['url'] =
- 'http://ppa.launchpad.net/project-calico/calico-3.14/ubuntu'
+ 'http://ppa.launchpad.net/project-calico/calico-3.15-python2/ubuntu'
 default['bcpc']['calico']['repo']['key'] = 'calico/release.key'
 
 # calicoctl
@@ -12,4 +12,4 @@ default['bcpc']['calico']['calicoctl']['remote']['file'] = 'calicoctl'
 default['bcpc']['calico']['calicoctl']['remote']['source'] =
  "#{default['bcpc']['web_server']['url']}/calicoctl"
 default['bcpc']['calico']['calicoctl']['remote']['checksum'] =
- '4e38c7e81653faf3659b0afddabde4dff736bb1b4cc59ebe238907a9641816a7'
+ '4600d5d7f08ed9cf479fc8fa87518dbbca32a473d0a4a212cfecb610c18216aa'
