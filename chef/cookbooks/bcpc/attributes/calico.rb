00cddf8ba167b87157b8450a73fae222f5415cc8
David Comay <dcomay@bloomberg.net>
2020-09-14 16:59:52 -0400

    Upgrade to Calico 3.15.3, etcd3gw 0.2.6
    Signed-off-by: David Comay <dcomay@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/calico.rb
+++ chef/cookbooks/bcpc/attributes/calico.rb
@@ -12,15 +12,4 @@ default['bcpc']['calico']['calicoctl']['remote']['file'] = 'calicoctl'
 default['bcpc']['calico']['calicoctl']['remote']['source'] =
  "#{default['bcpc']['web_server']['url']}/calicoctl"
 default['bcpc']['calico']['calicoctl']['remote']['checksum'] =
- '4600d5d7f08ed9cf479fc8fa87518dbbca32a473d0a4a212cfecb610c18216aa'
-
-# calico-felix
-#
-# Although neither VXLAN or IP in IP encapsulation is used here by Calico,
-# the change in https://github.com/projectcalico/felix/pull/2063 to address
-# a security issue (see Tigera Technical Advisory TTA-2019-002) prevents
-# instances from using VXLAN or IP in IP encapsulation themselves. As a
-# work-around for the VXLAN case, an alternate port can be set for Calico
-# to "use" which allows the use of the standard VXLAN port by instances
-# within OpenStack.
-default['bcpc']['calico']['calico-felix']['vxlan_port'] = 9
+ 'ab63a161ad9882b50531cd3835ed7cd090deda8ed18e9b5c3eb6fafebda09574'
--- chef/cookbooks/bcpc/attributes/etcd3gw.rb
+++ chef/cookbooks/bcpc/attributes/etcd3gw.rb
@@ -3,6 +3,6 @@
 ###############################################################################
 
 default['bcpc']['etcd3gw']['remote_file'] = {
-  'file' => 'etcd3gw-0.2.5.tar.gz',
-  'checksum' => 'a382ecbe9c15464d30520a802b9ca1e29cb06fd751393c80389566f1fd8747bf',
+  'file' => 'etcd3gw-0.2.6.tar.gz',
+  'checksum' => '4a765a382ae18ebd4fccbc1388d1ac5226db0540bccd1f081052600df89145fd',
 }
