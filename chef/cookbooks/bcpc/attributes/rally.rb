2d5a18a4cadf025ef059ff027d2db4fc301bb0a6
Mike Boruta <mboruta1@bloomberg.net>
2021-01-28 17:25:26 +0000

    Update rally to the latest versions
    Signed-off-by: Mike Boruta <mboruta1@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/rally.rb
+++ chef/cookbooks/bcpc/attributes/rally.rb
@@ -3,8 +3,8 @@
 ###############################################################################
 
 default['bcpc']['rally']['enabled'] = false
-default['bcpc']['rally']['rally_openstack']['version'] = '1.6.0'
-default['bcpc']['rally']['rally']['version'] = '2.1.0'
+default['bcpc']['rally']['rally_openstack']['version'] = '2.1.0'
+default['bcpc']['rally']['rally']['version'] = '3.2.0'
 default['bcpc']['rally']['ssl_verify'] = false
 default['bcpc']['rally']['conf_dir'] = '/etc/rally'
 default['bcpc']['rally']['home_dir'] = '/var/lib/rally'
