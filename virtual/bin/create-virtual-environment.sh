7e2e5ca2df54569cd201a933dc61b1b9db78c058
Justin Pacheco <justinjpacheco@gmail.com>
2021-03-11 09:39:59 -0500

    vagrant reload fix
      - cd into the virtual directory before running vagrant reload
    
    Signed-off-by: Justin Pacheco <justinjpacheco@gmail.com>

--- virtual/bin/create-virtual-environment.sh
+++ virtual/bin/create-virtual-environment.sh
@@ -71,5 +71,5 @@ if [ "${VAGRANT_DEFAULT_PROVIDER}" == "libvirt" ] ; then
     ansible -i "${ansible_dir}/inventory.yml" cloud -b -B 1 -P 0 -m shell -a "sleep 5 && reboot"
     ansible -i "${ansible_dir}/inventory.yml" cloud -m wait_for_connection -a "delay=15"
 else
-    vagrant reload
+    ( cd "${virtual_dir}"; vagrant reload )
 fi
