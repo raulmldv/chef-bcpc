c1695d78fd3f568745fc45c9058464f4c7bcf5ed
Justin Pacheco <jpacheco39@bloomberg.net>
2021-04-14 15:11:46 -0400

    added patched version of vif.py
      - addresses bug detailed here: chef/cookbooks/bcpc/recipes/nova-compute.rb
    
    Signed-off-by: Justin Pacheco <jpacheco39@bloomberg.net>

--- chef/cookbooks/bcpc/recipes/nova-compute.rb
+++ chef/cookbooks/bcpc/recipes/nova-compute.rb
@@ -221,6 +221,13 @@ template '/etc/nova/nova-compute.conf' do
   notifies :restart, 'service[nova-compute]', :immediately
 end
 
+# install patched vif.py for libvirt
+# https://bugs.launchpad.net/nova/+bug/1893263
+cookbook_file '/usr/lib/python2.7/dist-packages/nova/virt/libvirt/vif.py' do
+  source 'nova/vif.py'
+  notifies :restart, 'service[nova-compute]', :immediately
+end
+
 execute 'wait for compute host' do
   environment os_adminrc
   retries 15
