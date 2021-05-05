b0da8b8baddfdf35e9e5e05c01bd93e2b7219688
Tyler Stachecki <tstachecki@bloomberg.net>
2021-05-05 15:18:42 -0400

    memcached: Keep attributes in a common place
    Some memcached attributes were in a memcached-specific
    file, while others were lumped in the default attribute
    file.  Move them all to the same location for consistency.
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/memcached.rb
+++ chef/cookbooks/bcpc/attributes/memcached.rb
@@ -2,6 +2,12 @@
 # Memcached
 ###############################################################################
 
+# Enable memcached double verbose logging.
+default['bcpc']['memcached']['debug'] = false
+
+# Set number of memcached connections.
+default['bcpc']['memcached']['connections'] = 10240
+
 # Specifies memcached maximum limit of RAM to use for item
 # storage (in megabytes). Note carefully that this isn't a global memory limit
 default['bcpc']['memcached']['memory'] = 64
