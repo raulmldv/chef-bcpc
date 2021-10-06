e3d4fe2953a1c28d3e9ebd769ecc1d0a5d023f24
Tyler Stachecki <tstachecki@bloomberg.net>
2021-10-06 16:00:44 -0400

    ceph: Remove `ceph-deploy` workaround
    Requires ceph>=15.2.13:
    https://bugs.launchpad.net/ubuntu/+source/ceph/+bug/1892448
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/recipes/ceph-packages.rb
+++ chef/cookbooks/bcpc/recipes/ceph-packages.rb
@@ -26,17 +26,3 @@ package %w(
   ceph
   ceph-deploy
 )
-
-# workaround python3.8 deprecation of platform.linux_distribution.
-# ceph-deploy has not been rewired to workaround this, so we do it here.
-package 'python3-distro'
-
-cookbook_file '/usr/lib/python3/dist-packages/ceph_deploy/hosts/remotes.py' do
-  source 'ceph/remotes.py'
-  notifies :run, 'execute[py3compile-ceph-deploy]', :immediately
-end
-
-execute 'py3compile-ceph-deploy' do
-  action :nothing
-  command 'py3compile -p ceph-deploy'
-end
