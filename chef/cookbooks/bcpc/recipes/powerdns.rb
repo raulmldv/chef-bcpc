c17f497cb552329da1cb250a7f1402866b1a99ad
Justin Pacheco <jpacheco39@bloomberg.net>
2021-11-22 19:06:52 -0500

    added powerdns enable/disable
    Signed-off-by: Justin Pacheco <jpacheco39@bloomberg.net>

--- chef/cookbooks/bcpc/recipes/powerdns.rb
+++ chef/cookbooks/bcpc/recipes/powerdns.rb
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+return unless node['bcpc']['powerdns']['enabled']
+
 require 'ipaddress'
 
 region = node['bcpc']['cloud']['region']
