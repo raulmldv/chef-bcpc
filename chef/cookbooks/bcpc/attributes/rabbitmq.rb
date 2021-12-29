d21a4d42028449ff0d3da22b8c1d44d076f8b5a5
Tyler Stachecki <tstachecki@bloomberg.net>
2021-12-29 11:21:22 -0500

    rabbitmq: Consume from mainline Focal
    The rabbitmq packaged in Bionic had bugs/issues and was
    wildly out of date, so we pulled from backports.  That's
    no longer necessary, as the same version of RabbitMQ is
    now available natively via Focal.
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/rabbitmq.rb
+++ chef/cookbooks/bcpc/attributes/rabbitmq.rb
@@ -6,10 +6,6 @@
 default['bcpc']['rabbitmq']['source']['repo']['enabled'] = false
 default['bcpc']['rabbitmq']['source']['repo']['url'] = 'http://dl.bintray.com/rabbitmq/debian'
 
-# source rabbitmq from a different distribution
-default['bcpc']['rabbitmq']['source']['distribution']['enabled'] = true
-default['bcpc']['rabbitmq']['source']['distribution']['name'] = 'bionic-backports'
-
 # if changing this setting, you will need to reset Mnesia
 # on all RabbitMQ nodes in the cluster
 default['bcpc']['rabbitmq']['durable_queues'] = true
