c281cabc8f03cfc771aca0aee5ef6ac8651d6b77
David Comay <dcomay@bloomberg.net>
2022-03-28 12:31:41 -0400

    Upgrade to Calico 3.22.1
    Signed-off-by: David Comay <dcomay@bloomberg.net>

--- chef/cookbooks/bcpc/attributes/calico.rb
+++ chef/cookbooks/bcpc/attributes/calico.rb
@@ -4,7 +4,7 @@
 
 # calico apt repository
 default['bcpc']['calico']['repo']['url'] =
- 'http://ppa.launchpad.net/project-calico/calico-3.21/ubuntu'
+ 'http://ppa.launchpad.net/project-calico/calico-3.22/ubuntu'
 default['bcpc']['calico']['repo']['key'] = 'calico/release.key'
 
 # calicoctl
@@ -12,7 +12,7 @@ default['bcpc']['calico']['calicoctl']['remote']['file'] = 'calicoctl'
 default['bcpc']['calico']['calicoctl']['remote']['source'] =
  "#{default['bcpc']['web_server']['url']}/calicoctl"
 default['bcpc']['calico']['calicoctl']['remote']['checksum'] =
- 'd495edfc254e00f008ef6872422a31ef5f442a1ff96bcb724dd2df86ef75b7e3'
+ 'b42be4f7c053f209127a9b9a6945ce11ab3bc0725f2741f92380400b008c6111'
 
 # calico-felix
 default['bcpc']['calico']['felix']['failsafe']['inbound'] = [
