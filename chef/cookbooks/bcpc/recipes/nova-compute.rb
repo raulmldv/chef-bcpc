d002c76cb3d5d758decfcb1b5cafb5a2ff343f17
Tyler Stachecki <tstachecki@bloomberg.net>
2022-07-28 18:31:03 -0400

    Add feature to assist migration of `ceph-mons`
    This commit adds a feature to the Nova migration code to
    overwrite `ceph-mon` addresses in the domain's XML
    specification with those specified in the general section
    of `ceph.conf`.
    
    It does the same for newly created and refreshed Nova
    block device mappings and Cinder attachments as well.
    
    Together, this assists the IP address migration of `ceph-mon`
    nodes, especially for VMs which have a long uptime, by
    allowing an operator to reconfigure the set of `ceph-mon`
    IPs via a single live-migration of each VM in the cluster.
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/recipes/nova-compute.rb
+++ chef/cookbooks/bcpc/recipes/nova-compute.rb
@@ -259,6 +259,21 @@ cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/vif.py' do
   notifies :restart, 'service[nova-compute]', :delayed
 end
 
+# Add feature to rewrite monitor addresses in the domain XML using values from
+# ceph.conf instead to faciliate Ceph monitor migration exercises
+cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/migration.py' do
+  source 'nova/migration.py'
+  notifies :run, 'execute[py3compile-nova]', :immediately
+  notifies :restart, 'service[nova-compute]', :delayed
+end
+
+# Do the same for nova.block_device_mappings when refreshing connection_info
+cookbook_file '/usr/lib/python3/dist-packages/nova/virt/block_device.py' do
+  source 'nova/block_device.py'
+  notifies :run, 'execute[py3compile-nova]', :immediately
+  notifies :restart, 'service[nova-compute]', :delayed
+end
+
 execute 'py3compile-nova' do
   action :nothing
   command 'py3compile -p python3-nova'
