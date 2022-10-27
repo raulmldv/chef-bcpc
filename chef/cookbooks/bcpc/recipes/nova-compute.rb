1f3913759d28b2073e06d916290a3dacd6ff873c
Tyler Stachecki <tstachecki@bloomberg.net>
2022-10-27 08:54:00 -0400

    ceph: Revert `ceph-mon` migration support
    The changes to support `ceph-mon` migration are very
    invasive and require custom patches to several files. Thus,
    remove it from the tree at this time but leave it in a
    commit for reverting at a future point in time if needed.
    
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- chef/cookbooks/bcpc/recipes/nova-compute.rb
+++ chef/cookbooks/bcpc/recipes/nova-compute.rb
@@ -259,44 +259,11 @@ cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/vif.py' do
   notifies :restart, 'service[nova-compute]', :delayed
 end
 
-# Add feature to rewrite monitor addresses in the domain XML using values from
-# migration.conf instead to faciliate Ceph monitor migration exercises
-#
-# Also, ensure that the MTU is not changed during a live-migration.
-# https://bugs.launchpad.net/nova/+bug/1984009
-cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/migration.py' do
-  source 'nova/migration.py'
-  notifies :run, 'execute[py3compile-nova]', :immediately
-  notifies :restart, 'service[nova-compute]', :delayed
-end
-
-# Do the same for nova.block_device_mappings when refreshing connection_info
-cookbook_file '/usr/lib/python3/dist-packages/nova/virt/block_device.py' do
-  source 'nova/block_device.py'
-  notifies :run, 'execute[py3compile-nova]', :immediately
-  notifies :restart, 'service[nova-compute]', :delayed
-end
-
-# And do the same when using rbd_utils (used by the ImageBackend for net-new BDMs)
-cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/storage/rbd_utils.py' do
-  source 'nova/rbd_utils.py'
-  notifies :run, 'execute[py3compile-nova]', :immediately
-  notifies :restart, 'service[nova-compute]', :delayed
-end
-
 # (rendition of): https://review.opendev.org/c/openstack/nova/+/852002
 cookbook_file '/usr/lib/python3/dist-packages/nova/virt/libvirt/guest.py' do
   source 'nova/guest.py'
   notifies :run, 'execute[py3compile-nova]', :immediately
   notifies :restart, 'service[nova-compute]', :delayed
-end
-
-template '/etc/ceph/migration.conf' do
-  action node['bcpc']['ceph']['mon-migration']['enabled'] ? :create : :delete
-  source 'ceph/migration.conf.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
 end
 
 execute 'py3compile-nova' do
