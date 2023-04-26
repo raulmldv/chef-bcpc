2f402d57b286bb84f8e9e20298d9a5b06fac9a13
Tyler Stachecki <tstachecki@bloomberg.net>
2023-04-26 13:30:00 -0400

    Allow for multi-OS Packer box builds (#2128)
    As we prepare to transition into Ubuntu Jammy and split the
    management of compute and storage infrastructure, it is
    helpful to provide developers with the ability to build
    clusters with varying compositions of operating system
    releases (e.g., a Focal-fueled OpenStack cluster conjoined
    with a Jammy-fueled Ceph cluster).
    
    This commit works in that direction by changing the Packer
    tooling such that > 1 OS release worth of Packer box builds
    are issued without changing the existing toolchain much.

--- virtual/packer/bin/download-packer-box.sh
+++ virtual/packer/bin/download-packer-box.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2022, Bloomberg Finance L.P.
+# Copyright 2023, Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,38 +16,48 @@
 
 set -xe
 
-packer_dir=$(dirname "$(dirname "$0")")
+cd "$(dirname "$(dirname "$0")")"
+os_config_variables="config/variables.json"
+s3_variables="config/s3.json"
 
 # Check if an official base box is added to vagrant
-
-s3_variables="${packer_dir}/config/s3.json"
-config_variables="${packer_dir}/config/variables.json"
-
 S3_CONFIG_FILE=$(jq -r '.s3_config_file' "$s3_variables")
-DOWNLOAD_BUCKET=$(jq -r '.download_bucket' "$s3_variables")
-DOWNLOAD_PACKER_BOX=$(jq -r '.download_packer_box' "$s3_variables")
-OUTPUT_PACKER_BOX_NAME=$(jq -r '.output_packer_box_name' "$config_variables")
+BUCKET=$(jq -r '.bucket' "$s3_variables")
 
-if [ "$DOWNLOAD_BUCKET" == "null" ] \
-   || [ "$DOWNLOAD_PACKER_BOX" == "null" ] \
-   || [ "$OUTPUT_PACKER_BOX_NAME" == "null" ]; then
-    printf "One or more variables in %s are undefined.\n" "$config_variables"
+if [ "$BUCKET" == "null" ]; then
+    printf "One or more variables in %s are undefined.\n" "$s3_variables"
     exit 1
 fi
 
-#download base box from s3 storage
-if [ "$S3_CONFIG_FILE" == "null" ]; then
-    s3cmd get --force "${DOWNLOAD_BUCKET}/${DOWNLOAD_PACKER_BOX}" \
-        "${packer_dir}/download/${DOWNLOAD_PACKER_BOX}"
-else
-    s3cmd -c "${S3_CONFIG_FILE}" get --force \
-        "${DOWNLOAD_BUCKET}/${DOWNLOAD_PACKER_BOX}" \
-        "${packer_dir}/download/${DOWNLOAD_PACKER_BOX}"
+if [ -z "${VAGRANT_DEFAULT_PROVIDER}" ]; then
+    echo "VAGRANT_DEFAULT_PROVIDER is not defined"
+    exit 1
 fi
 
-# add the downloaded box as the output box in vagrant
-VAGRANT_VAGRANTFILE=Vagrantfile vagrant box add \
-                    --force \
-                    --clean \
-                    --name "$OUTPUT_PACKER_BOX_NAME" \
-                    file://"${packer_dir}/download/${DOWNLOAD_PACKER_BOX}"
+for OS_RELEASE in $(jq -r '. | keys[]' "${os_config_variables}"); do
+    OS_RELEASE_AND_PROVIDER="${OS_RELEASE}_${VAGRANT_DEFAULT_PROVIDER}"
+
+    # Generate configuration variables for this OS release
+    PACKER_BOX_NAME=$(jq -r ".[\"${OS_RELEASE}\"].output_packer_box_name" \
+        "${os_config_variables}")
+
+    # Download base box from s3 storage
+    mkdir -p "output-vagrant-${OS_RELEASE}"
+
+    if [ "$S3_CONFIG_FILE" == "null" ]; then
+        s3cmd get --force \
+            "${BUCKET}/bcc-ubuntu-${OS_RELEASE_AND_PROVIDER}.box" \
+            "output-vagrant-${OS_RELEASE}/package.box"
+    else
+        s3cmd -c "${S3_CONFIG_FILE}" get --force \
+            "${BUCKET}/bcc-ubuntu-${OS_RELEASE_AND_PROVIDER}.box" \
+            "output-vagrant-${OS_RELEASE}/package.box"
+    fi
+
+    # add the downloaded box as the output box in vagrant
+    VAGRANT_VAGRANTFILE=Vagrantfile vagrant box add \
+                        --force \
+                        --clean \
+                        --name "$PACKER_BOX_NAME" \
+                        "file://output-vagrant-${OS_RELEASE}/package.box"
+done
