1e44c1aa17adb5be428fa267d11fcec4302d917b
Chris Morgan <cmorgan2@bloomberg.net>
2023-05-09 14:21:25 -0400

    fix indentation

--- virtual/packer/bin/create-packer-box.sh
+++ virtual/packer/bin/create-packer-box.sh
@@ -21,13 +21,13 @@ function vagrant_box_add {
         printf "Checking for vagrant parallels plugins"
         parallels=$(vagrant plugin list | grep parallels || true)
         if [ -z "$parallels" ]; then
-			printf "Please install vagrant parallels plugin\n"
-			exit 1
-		fi
+            printf "Please install vagrant parallels plugin\n"
+            exit 1
+        fi
     fi
     box="$1"
     version="$2"
-	provider="$3"
+    provider="$3"
     vagrant box add \
             --force \
             --insecure "${box}" \
@@ -42,7 +42,7 @@ for OS_RELEASE in $(jq -r '. | keys[]' "${os_config_variables}"); do
     # Generate configuration variables for this OS release
     config_variables=$(mktemp -t variables-XXXXXXX.json)
     jq -r ".[\"${OS_RELEASE}\"]" "${os_config_variables}" \
-        > "${config_variables}"
+       > "${config_variables}"
 
     # Check if an official base box is added to vagrant
     BASE_BOX=$(jq -r '.base_box' "${config_variables}")
@@ -51,37 +51,37 @@ for OS_RELEASE in $(jq -r '. | keys[]' "${os_config_variables}"); do
     VAGRANT_CACERT=$(jq -r '.vagrant_cacert' "${config_variables}")
     PACKER_BOX_NAME=$(jq -r '.output_packer_box_name' "${config_variables}")
     if [ "$BASE_BOX" == "null" ] \
-        || [ "$BASE_BOX_VERSION" == "null" ] \
-        || [ "$PACKER_BOX_NAME" == "null" ]; then
+           || [ "$BASE_BOX_VERSION" == "null" ] \
+           || [ "$PACKER_BOX_NAME" == "null" ]; then
         printf "Variable(s) in %s are undefined.\n" "$config_variables"
         exit 1
     fi
     base_box_exists=$(
         vagrant box list --machine-readable |
-        grep -i "${BASE_BOX}.*${BASE_BOX_PROVIDER}.*${BASE_BOX_VERSION}" \
-        || true
-    )
+            grep -i "${BASE_BOX}.*${BASE_BOX_PROVIDER}.*${BASE_BOX_VERSION}" \
+                || true
+                   )
     if [ -z "$base_box_exists" ]; then
-		# only libvirt requires add and mutate, for the others it's
-		# the straighforward way
+        # only libvirt requires add and mutate, for the others it's
+        # the straighforward way
         if [ "$BASE_BOX_PROVIDER" != "libvirt" ]; then
-			CURL_CA_BUNDLE=${VAGRANT_CACERT:+$VAGRANT_CACERT} vagrant_box_add "$BASE_BOX" "$BASE_BOX_VERSION" "$BASE_BOX_PROVIDER"
-		else
-			CURL_CA_BUNDLE=${VAGRANT_CACERT:+$VAGRANT_CACERT} vagrant_box_add "$BASE_BOX" "$BASE_BOX_VERSION" "virtualbox"
-			printf "Checking for vagrant mutate"
-			mutate=$(vagrant plugin list | grep mutate)
-			if [ -z "$mutate" ]; then
-				printf "Vagrant mutate not found. "
-				printf "Install vagrant-mutate to allow base conversion\n"
-				exit 1
-			else
-				printf "Vagrant mutate found, mutating VirtualBox version\n"
-				vagrant mutate "${BASE_BOX}" --input-provider virtualbox \
-						libvirt
-			fi
+            CURL_CA_BUNDLE=${VAGRANT_CACERT:+$VAGRANT_CACERT} vagrant_box_add "$BASE_BOX" "$BASE_BOX_VERSION" "$BASE_BOX_PROVIDER"
+        else
+            CURL_CA_BUNDLE=${VAGRANT_CACERT:+$VAGRANT_CACERT} vagrant_box_add "$BASE_BOX" "$BASE_BOX_VERSION" "virtualbox"
+            printf "Checking for vagrant mutate"
+            mutate=$(vagrant plugin list | grep mutate)
+            if [ -z "$mutate" ]; then
+                printf "Vagrant mutate not found. "
+                printf "Install vagrant-mutate to allow base conversion\n"
+                exit 1
+            else
+                printf "Vagrant mutate found, mutating VirtualBox version\n"
+                vagrant mutate "${BASE_BOX}" --input-provider virtualbox \
+                        libvirt
+            fi
 
-		fi
-	fi
+        fi
+    fi
     # prevent vagrant-libvirt from failing if there's scrapnel lying around
     if [ "$VAGRANT_DEFAULT_PROVIDER" == "libvirt" ]; then
         virsh destroy output-vagrant_source || true
@@ -102,25 +102,25 @@ for OS_RELEASE in $(jq -r '. | keys[]' "${os_config_variables}"); do
     current_packer_ver=$(packer --version)
     required_packer_ver="1.4.0"
     lower_packer_ver=$(printf '%s\n' "$required_packer_ver" \
-                                    "$current_packer_ver" \
-                        | sort -V \
-                        | head -n1)
+                              "$current_packer_ver" \
+                           | sort -V \
+                           | head -n1)
     if [ "$lower_packer_ver" = "$required_packer_ver" ]; then
         VAGRANT_VAGRANTFILE=Vagrantfile packer build \
-                            --force \
-                            --on-error=abort \
-                            --var-file="${config_variables}" \
-                            "config/config.json"
+                                        --force \
+                                        --on-error=abort \
+                                        --var-file="${config_variables}" \
+                                        "config/config.json"
         mv "output-vagrant" "output-vagrant-${OS_RELEASE}"
         cd "output-vagrant-${OS_RELEASE}"
         VAGRANT_VAGRANTFILE=Vagrantfile vagrant box add \
-                            --force \
-                            --clean \
-                            --name "$PACKER_BOX_NAME" \
-                            file://package.box
+                                        --force \
+                                        --clean \
+                                        --name "$PACKER_BOX_NAME" \
+                                        file://package.box
     else
         printf "Packer version is too low. Please install at least %s\n." \
-            "$required_packer_ver"
+               "$required_packer_ver"
         exit 1
     fi
     popd
