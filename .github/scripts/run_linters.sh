667929e6ad00cd0189cae8a0168431c4d93677e0
Tyler Stachecki <tstachecki@bloomberg.net>
2023-06-18 12:20:57 -0400

    networking-calico: Remove patch included in v3.26
    Signed-off-by: Tyler Stachecki <tstachecki@bloomberg.net>

--- .github/scripts/run_linters.sh
+++ .github/scripts/run_linters.sh
@@ -33,7 +33,6 @@ function main {
         ! -path "./chef/cookbooks/bcpc/files/default/nova/hw.py" \
         ! -path "./chef/cookbooks/bcpc/files/default/nova/migration.py" \
         ! -path "./chef/cookbooks/bcpc/files/default/nova/vif.py" \
-        ! -path "./chef/cookbooks/bcpc/files/default/calico/config.py" \
         -print0 | xargs -0 -t flake8
     ansible-lint -x var-naming ansible/
     cookstyle --version && cookstyle --fail-level A
